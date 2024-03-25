9dee2a30c6aa50a375b4c22142aae04e2a4535e2
williamgephart <williamrgephart@gmail.com>
2024-03-25 19:14:36 -0400

    added

--- app/src/main/java/com/example/project2final/SignUp.java
+++ app/src/main/java/com/example/project2final/SignUp.java
@@ -34,7 +34,7 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_sign_up);
 
         initializeViews();
 

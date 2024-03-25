194e790b12ab289673c927144b8b21bc7efc745c
williamgephart <williamrgephart@gmail.com>
2024-03-25 19:23:55 -0400

    sign up name mismatch

--- app/src/main/java/com/example/project2final/MainActivity.java
+++ app/src/main/java/com/example/project2final/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
                 startActivity(intent);
             }
         });

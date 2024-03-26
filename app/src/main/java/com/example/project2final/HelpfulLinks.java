8e478721dae933037a2dcf4e2c8a3c819216fce8
williamgephart <williamrgephart@gmail.com>
2024-03-25 22:09:20 -0400

    Helpful Links

--- app/src/main/java/com/example/project2final/HelpfulLinks.java
+++ app/src/main/java/com/example/project2final/HelpfulLinks.java
@@ -2,7 +2,11 @@ package com.example.project2final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpfulLinks extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class HelpfulLinks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpful_links);
+
+        Button lifeLine = findViewById(R.id.button988);
+
+        lifeLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://988lifeline.org");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Button findTreatment = findViewById(R.id.buttonFindTreatment);
+
+        findTreatment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://findtreatment.gov");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Button caringForMentalHealth = findViewById(R.id.buttonCaringForMentalHealth);
+
+        caringForMentalHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://www.nimh.nih.gov/health/topics/caring-for-your-mental-health");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
